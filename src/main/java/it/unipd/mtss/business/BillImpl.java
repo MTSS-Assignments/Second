69898dee855b53c0dda593017563f4cfb5ab2bfb
LuBu-0 <luca_busacca@hotmail.com>
2022-05-19 23:44:15 +0200

    close #12

--- src/main/java/it/unipd/mtss/business/BillImpl.java
+++ src/main/java/it/unipd/mtss/business/BillImpl.java
@@ -6,9 +6,11 @@
 package it.unipd.mtss.business;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Random;
 
 import it.unipd.mtss.business.exception.BillException;
 import it.unipd.mtss.model.EItem;
@@ -18,17 +20,21 @@ public class BillImpl implements Bill {
 
     private List<EItem> _list;
     private User _user;
-    private LocalDate _date;
+    private LocalDateTime _date;
 
-    // costruttore
-    // public BillImpl(List<EItem> list, User user, LocalDate date){
-    // this._list = list;
-    // this._user = user;
-    // this._date = date;
-    // }
+    /**************************************************************************************
+    *****   Costruttore
+    **************************************************************************************/
+    public BillImpl(List<EItem> list, User user, LocalDateTime date){
+        this._list = list;
+        this._user = user;
+        this._date = date;
+    }
 
-    // Dato un elenco di articoli (Processori, Schede Madri, Tastiere, Mouse)
-    // calcolare il totale #1
+    /**************************************************************************************
+    *****   Dato un elenco di articoli (Processori, Schede Madri, Tastiere, Mouse)
+    *****   calcolare il totale #1
+    **************************************************************************************/
     private double totalPrice(List<EItem> itemsOrdered) throws BillException {
         double total = 0;
         if (itemsOrdered != null) {
@@ -41,8 +47,10 @@ public class BillImpl implements Bill {
         return total;
     }
 
-    // Se vengono ordinati più di 5 Processore viene fatto uno sconto del 50% sul
-    // prezzo del Processori meno caro #2
+    /**************************************************************************************
+    *****   Se vengono ordinati più di 5 Processore viene fatto uno sconto del 50% sul
+    *****   prezzo del Processori meno caro #2
+    **************************************************************************************/
     public static double scontoProcessori(List<EItem> itemsOrdered) {
         int count = 0;
         double cheapest = Double.POSITIVE_INFINITY;
@@ -62,8 +70,10 @@ public class BillImpl implements Bill {
         return 0;
     }
 
-    // Se vengono ordinati lo stesso numero di Mouse e Tastiere viene regalato
-    // l’articolo meno caro #4
+    /**************************************************************************************
+    *****   Se vengono ordinati lo stesso numero di Mouse e Tastiere viene regalato
+    *****   l’articolo meno caro #4
+    **************************************************************************************/
     public static double giftCheapest(List<EItem> itemsOrdered) {
         int countMouses = 0, countKeyboards = 0;
         double cheapest = Double.POSITIVE_INFINITY;
@@ -87,13 +97,43 @@ public class BillImpl implements Bill {
 
         return 0;
     }
-
-    // Non è possibile avere un’ordinazione con più di 30 elementi (se accade
-    // prevedere un messaggio d’errore) #6
+    /**************************************************************************************
+    *****    Non è possibile avere un’ordinazione con più di 30 elementi (se accade
+    *****    prevedere un messaggio d’errore) #6
+    **************************************************************************************/
     public static void maxThirty(List<EItem> itemsOrdered) throws BillException {
         if (itemsOrdered.size() > 30) {
             throw new BillException("Non è possibile avere un'ordinazione con più di 30 elementi");
         }
+    }
+
+    /*************************************************************************************
+    ****    Prevedere la possibilità di regalare, in modo casuale, 10 ordini effettuati
+    ****    dalle 18:00 alle 19:00 da utenti minorenni differenti. #8
+    **************************************************************************************/
+    public static double rndGift(List<BillImpl> todayReport) throws BillException {
+        int count = 10;
+        List<BillImpl> aux = new ArrayList<BillImpl>();
+
+        for (BillImpl it : todayReport) {
+            if (it._user.getDate_of_birth().isAfter(LocalDate.now().minus(18, ChronoUnit.YEARS))) {
+                aux.add(it);
+            }
+        }
+
+        int remained = aux.size();
+        double totale = 0;
+        
+
+        while(remained > 0 && count > 0) {
+            Random rand = new Random();
+            BillImpl randomElement = aux.get(rand.nextInt(remained));
+            totale += randomElement.getOrderPrice(randomElement._list, randomElement._user);
+            remained--;
+            count--;
+        }
+
+        return totale;
     }
 
     @Override
--- src/test/java/it/unipd/mtss/business/BillTest.java
+++ src/test/java/it/unipd/mtss/business/BillTest.java
@@ -6,6 +6,7 @@
 package it.unipd.mtss.business;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +27,7 @@ public class BillTest {
     private List<EItem> large = new ArrayList<EItem>();
 
     @Before
-    public void inizializeOrder() {
+    public void inizializeOrders() {
         order.add(new EItem(item.Processor, "Processore 2", 269.99));
         order.add(new EItem(item.Processor, "Processore 1", 250.00));
         order.add(new EItem(item.Processor, "Processore 3", 299.99));
@@ -83,6 +84,7 @@ public class BillTest {
         large.add(new EItem(item.Mouse, "Mouse 3", 49.99));
     }
 
+
     //  #1
     @Test
     public void testTotalPrice() {
@@ -90,9 +92,9 @@ public class BillTest {
         lista.add(new EItem(EItem.item.Processor, "processore", 100));
         lista.add(new EItem(EItem.item.Processor, "processore2", 100));
 
-        BillImpl impl = new BillImpl();
         LocalDate nascita = LocalDate.of(1997, 11, 30);
         User user = new User("Luca", "Busacca", nascita);
+        BillImpl impl = new BillImpl(order,user,LocalDateTime.of(1980, 01, 01, 19, 00, 00));
 
         try {
             impl.getOrderPrice(lista, user);
@@ -104,9 +106,9 @@ public class BillTest {
     @Test
     public void testTotalPriceNullList() {
         List<EItem> lista = null;
-        BillImpl impl = new BillImpl();
         LocalDate nascita = LocalDate.of(1997, 11, 30);
         User user = new User("Luca", "Busacca", nascita);
+        BillImpl impl = new BillImpl(order, user, LocalDateTime.of(1980,1,1,19,0,0));
 
         try {
             impl.getOrderPrice(lista, user);
@@ -156,4 +158,25 @@ public class BillTest {
         }
     }
 
+    //  #8
+    @Test
+    public void testRndGiftCount() throws BillException {
+        int count = 15;
+        List<BillImpl> todayReport = new ArrayList<BillImpl>();
+        while (count > 0) {
+            todayReport.add(new BillImpl (order1, new User("Cod","Ing",LocalDate.of(2008,01,01)), LocalDateTime.of(2022,05,19,19,05)));
+            todayReport.add(new BillImpl (order, new User("Cod","Ing",LocalDate.of(1980,01,01)), LocalDateTime.of(2022,05,22,15,25)));
+            count--;
+        }
+        double totale = BillImpl.rndGift(todayReport);
+        assertEquals(totale, 1199.9, 0.1);
+    }
+
+    @Test
+    public void testRndGiftRemained() throws BillException {
+        List<BillImpl> todayReport = new ArrayList<BillImpl>();
+        todayReport.add(new BillImpl (order1, new User("Cod","Ing",LocalDate.of(2008,01,01)), LocalDateTime.of(2022,05,19,19,05)));
+        double totale = BillImpl.rndGift(todayReport);
+        assertEquals(totale, 119.99, 0.1);
+    }
 }
